98af04ff3d432e832183afec486b32058f1b37fd
Nacho521 <juanignacioromero123456@gmail.com>
2024-10-21 18:54:42 -0300

    Update in params

--- src/main/java/models/Procedure.java
+++ src/main/java/models/Procedure.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 public class Procedure{
 
   private String nameProcedure; // Nombre del procedimiento.
-  private ArrayList<Parametro<String,Integer,String>> params; // Parametros del procedimiento.
+  private ArrayList<Parametro<String,String,String>> params; // Parametros del procedimiento.
 
-  public Procedure(String name){
+  public Procedure(String name,ArrayList<Parametro<String,String,String>> params_param){
     nameProcedure = name;
-    params = new  ArrayList<Parametro<String,Integer,String>>();
+    params = params_param;
   }
 
   public String getNameProcedure(){
@@ -19,12 +19,12 @@ public class Procedure{
     nameProcedure = new_name;
   }
 
-  public ArrayList<Parametro<String,Integer,String>> getParams(){
+  public ArrayList<Parametro<String,String,String>> getParams(){
     return params;
   }
 
-  public void setParams(String name,Integer tpye,String data){
-    Parametro<String,Integer,String> param = new Parametro<>(name,tpye,data);
+  public void setParams(String name,String tpye,String data){
+    Parametro<String,String,String> param = new Parametro<>(name,tpye,data);
     params.add(param);
   }
 
@@ -37,7 +37,7 @@ public class Procedure{
     return false;
   }
 
-  public boolean equalsParams(ArrayList<Parametro<String,Integer,String>> other_params){
+  public boolean equalsParams(ArrayList<Parametro<String,String,String>> other_params){
 
     for(int i = 0; i < params.size();i++){
       if(!params.get(i).getName().equals(other_params.get(i).getName()) || 
