aa0a57ae0838f786ae39c8efd7503a50946e8ccb
MatiasPellizzari <matiaspellizzari0@gmail.com>
2024-10-31 21:28:01 -0300

    Several changes, first idea to write the differences.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 import builder.DatabaseBuilder;
@@ -12,10 +13,10 @@ public class Main {
   
   public static void main(String[] args) {
     
-    String username = "santi";
-    String password = "pass";
-    String host = "192.168.1.3:3306";
-    String databaseName = "bdd2_ejercicio_1";
+    String username = "root";
+    String password = "root";
+    String host = "localhost:3306";
+    String databaseName = "proyectobdd2";
 
     DatabaseBuilder dbBuilder = new MySqlDatabaseBuilder(username, password, host, databaseName);
     DatabaseBuilder dbBuilder2 = new MySqlDatabaseBuilder(username, password, host, "bdd2_ejercicio_1_b");
@@ -24,6 +25,32 @@ public class Main {
 
     System.out.println(db1.equals(db2));
 
+    List<Table> tablesdb1 = db1.getTables();
+    List<Table> tablesdb2 = db2.getTables();
+    //Tablas que comparten nombre, van de a dos. Podemos usar tuplas
+    List<Table> equalnamedTables= new ArrayList<>();
+    //Tablas que NO comparten nombre, serian las que sobran.
+    List<Table> notequalTables= new ArrayList<>();
+
+    for(Table tabledb1: tablesdb1){
+      boolean equal= false;
+      for(Table tabledb2: tablesdb2){
+        //Si comparten nombre las agregamos a las que tienen que ser comparadas
+        if(tabledb1.getName().equals(tabledb2.getName())){
+          equalnamedTables.add(tabledb1);
+          equalnamedTables.add(tabledb2);
+          tablesdb2.remove(tabledb2);
+          equal=true;
+          break;
+        } 
+      }
+      //Si no se encontro ningun igual lo agregamos a notequal
+      if(!equal){
+        notequalTables.add(tabledb1);
+      }
+    }
+    //Agregamos las tablas que faltan de la db2, las que tienen nombre igual ya fueron eliminadas
+    notequalTables.addAll(tablesdb2);
   }
 
 }
--- src/main/java/models/Table.java
+++ src/main/java/models/Table.java
@@ -1,5 +1,6 @@
 package models;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Table {
@@ -56,6 +57,49 @@ public class Table {
           && type.equals(other.getType())
           && triggers.equals(other.getTriggers());
       }
+
+      public String WriteDifferences(Table table2){
+        String result = "Differences found: \\n";
+
+          //Comparacion de tipos
+          if(this.getType().equals(table2.getType())){
+            result= result + "-Different Types \\n" + //
+                              "type of the first column:" +this.getType()  + "\\n" +
+                              "type of the second column:" + table2.getType();              
+          }
+          //Comparacion de columnas
+          List<Column> columnstable1 = this.getColumns();
+          List<Column> columnstable2 = table2.getColumns();
+          //Columnas que comparten nombre, van de a dos. Podemos usar tuplas
+          List<Column> equalNamedColumns= new ArrayList<>();
+          //Tablas que NO comparten nombre, serian las que sobran.
+          List<Column> notequalColumns= new ArrayList<>();
+          for(Column column1: columnstable1){
+            boolean equal= false;
+            for(Column column2: columnstable2){
+              //Si comparten nombre las agregamos a las que tienen que ser comparadas
+              if(column1.getName().equals(column2.getName())){
+                equalNamedColumns.add(column1);
+                equalNamedColumns.add(column2);
+                columnstable2.remove(column2);
+                equal=true;
+                break;
+              } 
+            }
+            //Si no se encontro ningun igual lo agregamos a notequal
+            if(!equal){
+              notequalColumns.add(column1);
+            }
+          }
+          //Agregamos las tablas que faltan de la db2, las que tienen nombre igual ya fueron eliminadas
+          notequalColumns.addAll(columnstable2);
+        
+          
+        
+    
+        return result;
+      }
+
 
       public String toString() {
         String result = "{ " + "name " + name + 
